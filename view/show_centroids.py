a8f43942eb388ce33d19d98e112b5e86ad1b103c
Claudio Peroni <claudio.peroni93@gmail.com>
2017-08-27 17:59:13 +0200

    Documentation added for the function that shows 2D centroids.

--- view/show_centroids.py
+++ view/show_centroids.py
@@ -1,4 +1,7 @@
-"""2D representation of up to 3 centroids"""
+r"""2D representation of up to 4 centroids. Requires pylab.
+Also, should problems arise with the module, see
+<<https://stackoverflow.com/questions/27630114/matplotlib-issue-on-os-x-importerror-cannot-import-name-thread>>
+"""
 
 
 import pylab
@@ -6,9 +9,10 @@ import pylab
 
 def represent_2D_centroids(X, assign, centroids):
     pylab.cla()
+    # The following functions print up to 4 centroids with their respective assigned observations
     pylab.plot(X[assign == 0, 0], X[assign == 0, 1], '*b',
          X[assign == 1, 0], X[assign == 1, 1], '*r',
-         X[assign == 2, 0], X[assign == 2, 1], '*g')
+         X[assign == 2, 0], X[assign == 2, 1], '*g', X[assign == 3, 0], X[assign == 3, 1], '*y')
     pylab.plot(centroids[:, 0], centroids[:, 1], '*m', markersize=20)
     pylab.draw()
     pylab.ioff()
